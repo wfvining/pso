4ce764a202059561e9abe4f8f7978b71651d0b83
Will Vining <wfvining@gmail.com>
2021-03-11 21:17:24 -0700

    Add a `wait` state to the particle state machine
    Wait allows for external control of the particle's execution which
    will keep the counting of iterations and the other termination logic
    separated from the logic of moving, accelerating, and evaluating a
    particle. Does create a bit of a concern as to whether we are
    maximizing the available parallelism by having all particles update
    concurrently, but I don't believe this is much of a concern since the
    computationally expensive step (evaluating the objective function) is
    executed concurrently by all particles.
    
    Comments and documentation is also substantially improved, and some
    convenient type aliases are added.

--- src/particle.erl
+++ src/particle.erl
@@ -4,11 +4,14 @@
 %%% A particle has a poistion, a velocity, and a set of neighbors.
 %%%
 %%% Upon initialization a particle enters into a loop:
-%%% - the value at its current position is computed
-%%% - the particle sends its position and value to all its neighbors
-%%% - it awaits the current value and position from all other neighbors
-%%% - it updates its position and velocity according to the information
+%%% <ul>
+%%% <li> the value at its current position is computed </li>
+%%% <li> the particle waits to be instructed to continue </li>
+%%% <li> the particle sends its position and value to all its neighbors </li>
+%%% <li> it awaits the current value and position from all other neighbors </li>
+%%% <li> it updates its position and velocity according to the information </li>
 %%%   received from the neighbors.
+%%% </ul>
 %%% @end
 %%% Created : 21 Feb 2021 by Will Vining <wfvining@gmail.com>
 
@@ -16,12 +19,16 @@
 
 -behavior(gen_statem).
 
--export([start_link/3, assign_neighbors/2]).
+%% api functions
+-export([start_link/3, assign_neighbors/2, step/1]).
+%% gen_statem callbacks
 -export([init/1, callback_mode/0]).
--export([initialize/3, accelerate/3, eval/3]).
+%% state callbacks
+-export([initialize/3, accelerate/3, eval/3, wait/3]).
 
 -type velocity() :: term().
 -type position() :: term().
+-type value() :: term().
 
 -record(state, {position :: position(),
                 pbest :: position(),
@@ -29,9 +36,12 @@
                 velocity :: velocity(),
                 neighbors = [] :: list(pid()),
                 received = [] :: list({position(), term()}),
-                refs :: list(reference()),
-                value :: any(),
+                refs = [] :: list(reference()),
+                value :: value(),
                 module :: module()}).
+
+-type particle_state() :: #state{}.
+-type neighbor_state() :: {position(), value()}.
 
 %% Evaluate the objective function.
 -callback eval(Position :: any()) ->
@@ -42,24 +52,28 @@
 
 %% Acceleration requires the following three operations
 
-%% @doc
 %% Return `PositionA' minus `PositionB' (e.g. the displacement between
 %% the positions).
-%% @end
 -callback displacement(PositionA :: position(), PositionB :: position()) ->
     position().
 
-%% @doc Multiply `Velocity' by `Scale'
+%% Multiply `Velocity' by `Scale'
 -callback scale_velocity(Scale :: float(), Velocity :: velocity()) ->
     velocity().
 
-%% @doc Add two velocities.
+%% Add two velocities.
 -callback add_velocity(VelocityA :: velocity(), VelocityB :: velocity()) ->
     velocity().
 
-%% @doc Return true if `ValueA' is less than or equal to `ValueB'.
+%% Return true if `ValueA' is less than or equal to `ValueB'.
 -callback compare(ValueA :: any(), ValueB :: any()) -> boolean().
 
+%% @doc
+%% Start a particle and link to its process. The particle is initially
+%% at `Position' and moving at `Velocity'. `Module' is the callback
+%% module implementing the `particle' behavior.
+%% @end
+-spec start_link(position(), velocity(), module()) -> {ok, pid()}.
 start_link(Position, Velocity, Module) ->
     gen_statem:start_link(?MODULE, {Position, Velocity, Module}, []).
 
@@ -67,6 +81,11 @@ start_link(Position, Velocity, Module) ->
 -spec assign_neighbors(Particle :: pid(), Neighbors :: list(pid())) -> ok.
 assign_neighbors(Particle, Neighbors) ->
     gen_statem:cast(Particle, {neighbors, Neighbors}).
+
+%% @doc Step the particle through a single iteration of the PSO algorithm.
+-spec step(Particle :: pid()) -> ok.
+step(Particle) ->
+    gen_statem:cast(Particle, continue).
 
 callback_mode() ->
     [state_functions, state_enter].
@@ -76,6 +95,9 @@ init({Position, Velocity, Module}) ->
                              velocity = Velocity,
                              module = Module}}.
 
+%% Apply the PSO acceleration algorithm to the particle state.
+-spec accelerate_particle(Data :: particle_state(), list(neighbor_state()))
+                         -> velocity().
 accelerate_particle(#state{pbest = PBest,
                            position = Position,
                            module = Module,
@@ -95,6 +117,11 @@ accelerate_particle(#state{pbest = PBest,
         Module:scale_velocity(2*rand:uniform(), BestDisplacement)),
       Module:scale_velocity(2*rand:uniform(), NeighborhoodDisplacement)).
 
+%% @doc
+%% Particles begin executing in this state. Here they wait for a list
+%% of neighboring particle PIDs to be provided, then for a singal that
+%% they should begin execution.
+%% @end
 initialize(enter, _, Data) ->
     {keep_state, Data};
 initialize(cast, start, Data) ->
@@ -103,23 +130,47 @@ initialize(cast, {neighbors, Neighbors}, Data) ->
     Refs = [monitor(process, Neighbor) || Neighbor <- Neighbors],
     {keep_state, Data#state{ neighbors = Neighbors, refs = Refs }}.
 
+%% @doc
+%% Upon entry into this state the position is evaluated and the
+%% particle is moved into the `wait' state.
+%% @end
+%% TODO can this be folded in to the `wait' state?
 eval(enter, initialize, Data = #state{ position = Position,
                                        module = Module }) ->
     Value = Module:eval(Position),
-    {next_state, accelerate, Data#state{value = Value, pbest = Position}};
+    {next_state, wait, Data#state{value = Value, pbest = Position}};
 eval(enter, _OldState, Data = #state{ position = Position,
                                       vbest = VBest,
                                       module = Module }) ->
     Value = Module:eval(Position),
     case Module:compare(VBest, Value) of
         true ->
-            {next_state, accelerate, Data#state{ value = Value,
-                                                 pbest = Position,
-                                                 vbest = Value }};
+            {next_state, wait, Data#state{ value = Value,
+                                           pbest = Position,
+                                           vbest = Value }};
         false ->
-            {next_state, accelerate, Data#state{ value = Value }}
+            {next_state, wait, Data#state{ value = Value }}
     end.
 
+%% @doc
+%% Wait for a signal to continue before accelerating the particle.
+%% @end
+wait(enter, _, Data) ->
+    {keep_state, Data};
+wait(cast, continue, Data) ->
+    {next_state, accelerate, Data};
+wait(cast, {value, _, _}, Data) ->
+    % Postpone values received from neighboring particles until the
+    % `accelerate' state.
+    {keep_state, Data, {postpone, true}}.
+
+%% @doc
+%% Upon entry into the accelerate state the particle reports its
+%% current position and value to its neighboring particles. The
+%% particle then waits until all its neighbors have reported their
+%% position and value at which point its position is updated and it
+%% is accelerated.
+%% @end
 accelerate(enter, _OldState, Data = #state{position = Position,
                                            value = Value}) ->
     lists:foreach(
@@ -137,6 +188,10 @@ accelerate(cast, {value, Position, Value},
         NewReceived when length(NewReceived) =:= length(Neighbors) ->
             {next_state, eval,
              Data#state{ received = [],
+                         % XXX this returns a velocity, not a
+                         % position. The updated velocity should be
+                         % applied to the current position prior to
+                         % the evaluation of the new position.
                          position = accelerate_particle(Data, NewReceived)}};
         NewReceived ->
             {keep_state, Data#state{received = NewReceived}}
