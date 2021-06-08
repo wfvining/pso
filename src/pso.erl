244492039fc5ac463dfea1383e2cc7b0bbe21390
Will Vining <wfvining@gmail.com>
2021-06-07 20:37:10 -0600

    Update pso.erl to use the particle_server API
    Rather than directly managing each particle, pso:run/5 spawns a
    particle_server for each particle, requests each server to iterate,
    and waits for the iteration to finish.

--- src/pso.erl
+++ src/pso.erl
@@ -4,7 +4,6 @@
 
 -type position() :: particle:position().
 -type value() :: particle:value().
--type particle() :: particle:particle().
 
 %% @doc
 %%
@@ -16,36 +15,45 @@
           InitialPositions :: [position()],
           InitialVelocities :: [particle:velocity()],
           Network :: network:network(),
-          Iterations :: pos_integer()) -> {position(), value()}.
+          Iterations :: pos_integer()) -> particle:state().
 run(ObjectiveFun, InitialPositions, InitialVelocities, Network, Iterations) ->
     Particles = maps:from_list(
-                  [{N, particle:new(Pos, Vel, ObjectiveFun)}
+                  [{N,
+                    element(2, particle_server:start_link(Pos, Vel, ObjectiveFun, []))}
                    || {N, Pos, Vel} <- lists:zip3(network:all_nodes(Network),
                                                   InitialPositions,
                                                   InitialVelocities)]),
-    best_solution(maps:values(iterate(Particles, Network, Iterations))).
+    %% Set up the neighbors.
+    maps_foreach(
+      fun (N, ParticleServer) ->
+              particle_server:set_neighbors(
+                ParticleServer,
+                [maps:get(Neighbor, Particles)
+                 || Neighbor <- network:neighbors(N, Network)])
+      end, Particles),
 
--spec best_solution(Particles :: [particle()]) -> {position(), value()}.
-best_solution(Particles) ->
-    BestParticle =
-        hd(lists:sort(
-             fun(ParticleA, ParticleB) ->
-                     particle:value(ParticleA) < particle:value(ParticleB)
-             end,
-             Particles)),
-    {particle:position(BestParticle), particle:value(BestParticle)}.
+    %% Kick off the iteration.
+    maps_foreach(
+      fun (_, ParticleServer) ->
+              particle_server:eval(ParticleServer, Iterations)
+      end, Particles),
 
--spec iterate(Particles :: #{term() := particle()},
-              Network :: network:network(),
-              Iterations :: non_neg_integer()) -> #{term() := particle()}.
-iterate(Particles, _, 0) ->
-    Particles;
-iterate(Particles, Network, Iterations) ->
-    NewParticles =
-        maps:map(
-          fun(N, Particle) ->
-                  particle:step(Particle,
-                                [particle:state(maps:get(P, Particles))
-                                 || P <- network:neighbors(N, Network)])
-          end, Particles),
-    iterate(NewParticles, Network, Iterations - 1).
+    %% Get the best solution
+    Best = best_solution(maps:values(Particles), Iterations),
+
+    %% Stop the servers
+    lists:foreach(fun particle_server:stop/1, maps:values(Particles)),
+
+    Best.
+
+
+-spec maps_foreach(Fun :: fun((Key, Value) -> any()),
+                   Map :: #{Key := Value}) -> any().
+maps_foreach(Fun, Map) ->
+    maps:fold(fun (Key, Value, _) -> Fun(Key, Value) end, nil, Map).
+
+-spec best_solution(Particles :: [pid()], After :: pos_integer())
+                   -> particle:state().
+best_solution(Particles, After) ->
+    hd(lists:keysort(2, [particle_server:state(Particle, After)
+                         || Particle <- Particles])).
