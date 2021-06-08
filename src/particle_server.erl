c04a77d37a79316df2e2723af7ed45b7df792d72
Will Vining <wfvining@gmail.com>
2021-06-07 20:13:30 -0600

    Make particle_server:eval/2 a gen_server:call
    Using a call instead of a cast lets us return an indication of how
    many total iterations will have been completed once the iteration is
    done (for example, if the server has already completed some
    iterations), and it lets us signal to the caller if the server is
    already iterating.
    
    Add particle_server:stop/1 to the API.
    
    Add particle_server:state/2 to the API. This lets us block the caller
    until some number of iterations have been completed. It requires
    adding another field to the server state to store requests that have
    not been completed yet. The stored requests are scanned each time an
    iteration is completed.
    
    Bug fixed: assign list of neigbors _before_ trying to send the initial
    state to all neighbors when handling the {assing_neighbors, _} cast.

--- src/particle_server.erl
+++ src/particle_server.erl
@@ -17,10 +17,10 @@
 
 -behaviour(gen_server).
 
--export([init/1, handle_cast/2, handle_call/3, handle_info/2, code_change/3]).
+-export([init/1, handle_cast/2, handle_call/3, handle_info/2, code_change/3,
+         terminate/2]).
 
--export([eval/2, state/1, set_neighbors/2,
-         start_link/4, report_state/2]).
+-export([eval/2, state/1, state/2, set_neighbors/2, start_link/4, stop/1]).
 
 -record(state, {particle :: particle:particle(),
                 neighbors = [] :: [pid()],
@@ -28,7 +28,8 @@
                 neighbor_state = #{} :: #{ pid() => particle:state()},
                 iteration = 1 :: pos_integer(),
                 stop_iteration = 1 :: pos_integer(),
-                evaluator = idle :: idle | pid()}).
+                evaluator = idle :: idle | pid(),
+                pending_requests = #{} :: #{pos_integer() := [{pid(), term()}]}}).
 
 -type state() :: #state{}.
 
@@ -56,24 +57,44 @@ start_link(Position, Velocity, ObjectiveFun, Options) ->
               [])
     end.
 
--spec eval(ParticleServer :: pid(), Iterations :: pos_integer()) -> ok.
+%% @doc
+%% Evaluate `Iterations' interations of the PSO algorithm. The
+%% iteration number of the final iteration that will be evaluated is
+%% returned. (For example, if the server has already evaluated 10
+%% iterations, and we request a further 12 iterations, ``{ok, 22}'' is
+%% returned.)
+%% @end
+-spec eval(ParticleServer :: pid(), Iterations :: pos_integer())
+          -> {ok, FinalIteration :: pos_integer()}
+              | {already_iterating, FinalIteration :: pos_integer()}.
 eval(ParticleServer, Iterations) ->
-    gen_server:cast(ParticleServer, {eval, Iterations}).
+    gen_server:call(ParticleServer, {eval, Iterations}).
 
+%% @doc Get the current state of the particle managed by this server.
 -spec state(ParticleServer :: pid()) -> particle:state().
 state(ParticleServer) ->
     gen_server:call(ParticleServer, get_state).
 
+%% @doc
+%% Get the state of the particle after `Iteration' iterations have
+%% been completed. The returned state may be from any iteration
+%% *after* `Iteration', but not before. If `Iteration' has not been
+%% reached this call will block until it has been completed.
+%% @end
+-spec state(ParticleServer :: pid(), Iteration :: pos_integer())
+           -> particle:state().
+state(ParticleServer, Iteration) ->
+    gen_server:call(ParticleServer, {get_state, Iteration}, infinity).
+
+%% @doc Stop the server.
+-spec stop(ParticleServer :: pid()) -> ok.
+stop(ParticleServer) ->
+    gen_server:stop(ParticleServer).
+
+%% @doc Set the particle server's neighbors.
 -spec set_neighbors(ParticleServer :: pid(), Neighbors :: [pid()]) -> ok.
 set_neighbors(ParticleServer, Neighbors) ->
     gen_server:cast(ParticleServer, {set_neighbors, Neighbors}).
-
--spec report_state(ParticleServer :: pid(), State :: state()) -> ok.
-report_state(ParticleServer,
-             #state{ particle = Particle, iteration = Iteration }) ->
-    gen_server:cast(
-      ParticleServer,
-      {report_state, self(), particle:state(Particle), Iteration}).
 
 init(ParticleArgs) ->
     {ok,
@@ -84,19 +105,22 @@ handle_call({eval, _}, _From, State = #state{iteration = Iteration,
                                              stop_iteration = StopIteration})
   when Iteration < StopIteration ->
     {reply, {already_iterating, StopIteration}, State};
-handle_call({eval, Iterations}, From, State = #state{ iteration = Iteration}) ->
-    % Set the stop_iteration, then do a try_iteration.
-    % Since I want to abstract the try_iteration pattern, I'm going to use
-    % gen_server:reply/2, and then return a `{noreply, ...}' tuple.
+handle_call({eval, Iterations}, _From,
+            State = #state{ iteration = Iteration}) ->
     StopIteration = Iteration + Iterations,
-    gen_server:reply(From, {ok, StopIteration}),
-    NewState = State#state{stop_iteration = StopIteration},
-    case try_iteration(NewState) of
-        not_ready ->
-            {noreply, NewState};
-        {ok, Evaluator} ->
-            {noreply, NewState#state{ evaluator = Evaluator }}
-    end;
+    {reply, {ok, StopIteration},
+     try_iteration(State#state{stop_iteration = StopIteration})};
+handle_call({get_state, AtIteration}, _From,
+            State = #state{ iteration = Iteration,
+                            particle = Particle })
+  when Iteration >= AtIteration ->
+    %% Already at or past the requested iteration, return immediately.
+    {reply, particle:state(Particle), State};
+handle_call({get_state, AtIteration}, From,
+            State = #state{ pending_requests = PendingRequests}) ->
+    {noreply,
+     State#state{
+       pending_requests = add_pending_request(AtIteration, From, PendingRequests)}};
 handle_call(get_state, _From, State = #state{particle = Particle}) ->
     {reply, particle:state(Particle), State}.
 
@@ -107,17 +131,13 @@ handle_cast({set_neighbors, Neighbors},
     %% be left unspecified. By leaving the state unspecified an error
     %% will be raised if we try to get the state before a
     NewClocks = maps:from_list([{N, 0} || N <- Neighbors]),
-    report_state(State),
     NewState = State#state{
-                %% Discard any information about neighbors not in `Neighbors'
-                neighbor_clock = maps:with(Neighbors, maps:merge(Clocks, NewClocks)),
-                neighbor_state = maps:with(Neighbors, NeighborState)},
-    case try_iteration(NewState) of
-        not_ready ->
-            {noreply, NewState};
-        {ok, Evaluator} ->
-            {noreply, NewState#state{ evaluator = Evaluator }}
-    end;
+                 neighbors = Neighbors,
+                 %% Discard any information about neighbors not in `Neighbors'
+                 neighbor_clock = maps:with(Neighbors, maps:merge(Clocks, NewClocks)),
+                 neighbor_state = maps:with(Neighbors, NeighborState)},
+    report_state_to_neighbors(NewState),
+    {noreply, try_iteration(NewState)};
 handle_cast({report_state, Neighbor, NeighborState, Iteration},
             State = #state{ neighbor_clock = Clocks,
                             neighbor_state = Neighbors}) ->
@@ -138,50 +158,67 @@ handle_cast({report_state, Neighbor, NeighborState, Iteration},
                      Neighbors),
     NewState = State#state{ neighbor_clock = NewClocks,
                             neighbor_state = NewNeighbors },
-    case try_iteration(NewState) of
-        not_ready ->
-            {noreply, NewState};
-        {ok, Evaluator} ->
-            {noreply, NewState#state{ evaluator = Evaluator }}
-    end.
+    {noreply, try_iteration(NewState)}.
 
 %% Evaluator finished evaluating the next iteration. Increment the
 %% current iteration and replace the particle state with the new
 %% particle state.
 handle_info({evaluation_result, Evaluator, NewParticle},
-            State = #state{ evaluator = Evaluator, iteration = Iteration }) ->
+            State = #state{ evaluator = Evaluator,
+                            iteration = Iteration,
+                            pending_requests = PendingRequests }) ->
     NewState = State#state{ particle = NewParticle,
                             evaluator = idle,
                             iteration = Iteration + 1 },
-    report_state(NewState),
-    case try_iteration(NewState) of
-        not_ready ->
-            {noreply, NewState};
-        {ok, NewEvaluator} ->
-            {noreply, NewState#state{ evaluator = NewEvaluator}}
-    end.
+    report_state_to_neighbors(NewState),
+    handle_pending_requests(
+      Iteration + 1, particle:state(NewParticle), PendingRequests),
+    {noreply, try_iteration(NewState)}.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%% Report the particle's state to its neighbors.
-report_state(State = #state{ neighbors = Neighbors }) ->
-    lists:foreach(fun(N) -> report_state(N, State) end, Neighbors).
+terminate(_Reason, #state{evaluator = idle}) -> ok;
+terminate(Reason, #state{evaluator = Evaluator}) ->
+    %% If there is an evaluator running, unlink from it and kill it
+    %% with `Reason'.
+    erlang:unlink(Evaluator),
+    exit(Evaluator, Reason).
 
-%% If all neighbors are accounted for at the current time, then
-%% evaluate an iteration of the PSO algorithm.
--spec try_iteration(state()) -> not_ready | {ok, pid()}.
-try_iteration(#state{ evaluator = Evaluator }) when is_pid(Evaluator) ->
-    not_ready;
-try_iteration(#state{ neighbors = [] }) ->
-    not_ready;
-try_iteration(#state{ neighbors = Neighbors,
-                      neighbor_clock = Clocks,
-                      neighbor_state = NeighborState,
-                      iteration = CurrentIteration,
-                      stop_iteration = StopIteration,
-                      particle = Particle,
-                      evaluator = idle })
+%% @doc Report the `Iteration' and state of `Particle' to `ParticleServer'.
+-spec report_state(ParticleServer :: pid(),
+                   Particle :: particle:particle(),
+                   Iteration :: pos_integer()) -> ok.
+report_state(ParticleServer, Particle, Iteration) ->
+    gen_server:cast(
+      ParticleServer,
+      {report_state, self(), particle:state(Particle), Iteration}).
+
+%% @doc Report the particle's state to all of its neighbors.
+-spec report_state_to_neighbors(State :: state()) -> ok.
+report_state_to_neighbors(#state{ neighbors = Neighbors,
+                                  particle = Particle,
+                                  iteration = Iteration }) ->
+    lists:foreach(fun(N) -> report_state(N, Particle, Iteration) end, Neighbors).
+
+%% @doc
+%% If the state of all neighbors at the current current iteration is
+%% known, then spawn a new process to evaluate the next iteration of
+%% the PSO algorithm. If an evaluator is already running, or no
+%% neighbors have been assigned then no iteration is performed.
+%% @end
+-spec try_iteration(state()) -> state().
+try_iteration(State = #state{ evaluator = Evaluator }) when is_pid(Evaluator) ->
+    State;
+try_iteration(State = #state{ neighbors = [] }) ->
+    State;
+try_iteration(State = #state{ neighbors = Neighbors,
+                              neighbor_clock = Clocks,
+                              neighbor_state = NeighborState,
+                              iteration = CurrentIteration,
+                              stop_iteration = StopIteration,
+                              particle = Particle,
+                              evaluator = idle })
   when StopIteration > CurrentIteration ->
     Ready = lists:all(
               fun(Neighbor) ->
@@ -189,15 +226,48 @@ try_iteration(#state{ neighbors = Neighbors,
               end, Neighbors),
     if Ready ->
             Server = self(),
-            {ok,
-             spawn_link(
-               fun() ->
-                       NewParticle =
-                           particle:step(
-                             Particle,
-                             maps:values(maps:with(Neighbors, NeighborState))),
-                       Server ! {evaluation_result, self(), NewParticle}
-               end)};
+            State#state{
+              evaluator =
+                  spawn_link(
+                    fun() ->
+                            NewParticle =
+                                particle:step(
+                                  Particle,
+                                  maps:values(maps:with(Neighbors, NeighborState))),
+                            Server ! {evaluation_result, self(), NewParticle}
+                    end)};
        not(Ready) ->
-            not_ready
+            State
+    end;
+try_iteration(State = #state{ iteration = CurrentIteration,
+                              stop_iteration = StopIteration})
+  when StopIteration =:= CurrentIteration ->
+    State.
+
+
+-spec add_pending_request(At :: pos_integer(),
+                          Requestor :: {pid(), Tag},
+                          PendingRequests)
+                         -> PendingRequests
+              when PendingRequests :: #{ pos_integer() := [{pid(), Tag}]}.
+add_pending_request(At, Requestor, PendingRequests) ->
+    maps:update_with(
+      At,
+      fun (Requestors) -> [Requestor|Requestors] end,
+      [Requestor],
+      PendingRequests).
+
+-spec handle_pending_requests(At :: pos_integer(),
+                              ParticleState :: particle:state(),
+                              PendingRequests) -> PendingRequests
+              when PendingRequests :: #{pos_integer() := [{pid(), term()}]}.
+handle_pending_requests(At, ParticleState, PendingRequests) ->
+    case maps:take(At, PendingRequests) of
+        error ->
+            PendingRequests;
+        {Requestors, NewPendingRequests} ->
+            lists:foreach(
+              fun (Caller) -> gen_server:reply(Caller, ParticleState) end,
+              Requestors),
+            NewPendingRequests
     end.
